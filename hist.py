7101696f0d6b623aa756b6730090d653febcc701
Abhi Kannan <a.kannansundhari@med.miami.edu>
2020-02-28 10:40:18 -0500

    final script?too many claims?

--- hist.py
+++ hist.py
@@ -1,9 +1,17 @@
 #randomizing data, generating summary statistics, and histogram
 
 import numpy as np
-
+import matplotlib.pyplot as plt
 mu = 80
 sigma = 10
 
 x = np.random.normal(mu, sigma, 100) 
+
+print("Random Normal Array Mean Centered", x[:10])
+
+print("mean", np.mean(x))
+plt.hist(x)
+plt.show
+
+print("I am a champion of science")
 
